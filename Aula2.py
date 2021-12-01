342e5fbd1ddc63bf1d65130439a1662692f4cd46
Gabriel Newton <gabrielnewton.dev@gmail.com>
2021-11-30 21:25:45 -0300

    Add files via upload
    print("iremos verificar os demais operadores Aritimeticos")
    a = int(input("Entre com o primeiro valor: ")) #não consigo fazer uma soma de um texto por esse motivo utilizo o int()para conerter para inteiro
    b = int(input("Entre com o segundo valor: "))
    
    soma = a + b
    subtracao = a - b
    multiplicacao = a * b
    divisao = a / b
    resto = a % b
    #print("A soma é : {}. A subtração é : {}. A Multiplicação é : {}. A divisão é : {}. " .format(soma,subtracao,multiplicacao,divisao) #outra forma de converter a variavel ele print("A soma é: " + str(soma)) #para converter número inteiro em string (str) concatenou
    print("A Subtração é: " + str(subtracao))
    print("A multiplicação é :" + str(multiplicacao))
    print("A divisão é: " + str(divisao)) #para converter de float para int, utiliza o int. ex: print(int(divisão))
    print("O resto é : " + str(resto))
    print(type(divisao)) #type para saber qual é o tipo da variavel
    x = "1"
    soma2 = int(x) + 1
    print(soma2)

---
+++ Aula2.py
@@ -0,0 +1,19 @@
+print("iremos verificar os demais operadores Aritimeticos")
+a = int(input("Entre com o primeiro valor: ")) #não consigo fazer uma soma de um texto por esse motivo utilizo o int()para conerter para inteiro
+b = int(input("Entre com o segundo valor: "))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+#print("A soma é : {}. A subtração é : {}. A Multiplicação é : {}. A divisão é : {}. " .format(soma,subtracao,multiplicacao,divisao) #outra forma de converter a variavel ele print("A soma é: " + str(soma)) #para converter número inteiro em string (str) concatenou
+print("A Subtração é: " + str(subtracao))
+print("A multiplicação é :" + str(multiplicacao))
+print("A divisão é: " + str(divisao)) #para converter de float para int, utiliza o int. ex: print(int(divisão))
+print("O resto é : " + str(resto))
+print(type(divisao)) #type para saber qual é o tipo da variavel
+x = "1"
+soma2 = int(x) + 1
+print(soma2)
+
